fa37f9131c4c678a2ade8998b3944ee342cb8144
mignonstyle <mignonxstyle@gmail.com>
2017-10-15 23:35:51 +0900

    add wp_favicon_request()

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -142,4 +142,37 @@ function wp_check_php_mysql_versions() {
 	}
 }
 
+/**
+ * Don't load all of WordPress when handling a favicon.icon request.
+ *
+ * Instead, send the headers for a zoro-length favicon and bail.
+ *
+ * @since 3.0.0
+ */
+function wp_favicon_request() {
+	if ( '/favicon.ico' == $_SERVER['REQUEST_URI'] ) {
+		header('Content-Type: image/vnd.microsoft.icon');
+		exit;
+	}
+}
+
+/**
+ * Die with a maintenance message when conditions are met.
+ * Checks for a file in the WordPress root directory named ".maintenance".
+ * This file will contain the variable $upgrading, set to the time the file
+ * was created. If the file was created less than 10 minutes ago, WordPress
+ * enters maintenance mode and displays a message.
+ *
+ * The default message can be replaced by using a drop-in (maintenance.php in
+ * the wp-content directory).
+ *
+ * @since 3.0.0
+ * @access private
+ *
+ * @global int $upgrading the unix timestamp marking when upgrading WordPress began.
+ */
+
+
+
+
 echo('hogehoge');
