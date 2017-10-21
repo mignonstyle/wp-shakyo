57dd3a594018d347455a0c4fff1d3094af530915
mignonstyle <mignonxstyle@gmail.com>
2017-10-21 22:35:20 +0900

    add load.php doctype html

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -212,7 +212,6 @@ function wp_maintenance() {
 	header( 'Content-Type: text/html; charset=utf-8' );
 	header( 'Retry-After: 600' );
 ?>
-
 	<!DOCTYPE html>
 	<html xmlns="http://www.w3.org/1999/xhtml"<?php if ( is_rtl() ) echo 'dir="rtl"'; ?>>
 	<head>
@@ -220,6 +219,12 @@ function wp_maintenance() {
 		<title><?php _e( 'Maintenance' ); ?></title>
 
 	</head>
+	<body>
+		<h1><?php _e( 'Briefly unavailable for scheduled maintenance. Check back in a minute.' ); ?></h1>
+	</body>
+	</html>
+<?php
+	die();
 }
 
 
