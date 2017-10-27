37c237c46a7d0b5c098f2ad64dd1b3578aa6b75d
mignonstyle <mignonxstyle@gmail.com>
2017-10-27 22:26:14 +0900

    add load.php comment

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -272,6 +272,14 @@ function timer_stop( $display = 0, $precision = 3 ) {
  * Set PHP error reporting based on WordPress debug settings.
  *
  * Uses three constants: 'WP_DEBUG', 'WP_DEBUG_DESPLAY', and 'WP_DEBUG_LOG'.
+ * All three can be defined in wp-config.php By default, 'WP_DEBUG' and
+ * 'WP_DEBUG_LOG' are set to false, and 'WP_DEBUG_DISPLAY' is set to true.
+ *
+ * When 'WP_DEBUG' is true, all PHP notices are reported. WordPress will also
+ * display internal notices: when a deprecated WordPress function, function
+ * argument, or file is used. Deprecated code may be removed from a later
+ * varsion.
+ *
  */
 
 
